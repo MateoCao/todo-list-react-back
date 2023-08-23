bee967e304401a8addd31454658683f38775af9e
Mateo Cao <mateocao12@gmail.com>
2023-08-23 16:08:17 -0300

    cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 const ACCEPTED_ORIGINS = [
   'http://127.0.0.1:5500',
   'http://localhost:5173',
-  'https://main--effortless-custard-7e0ce8.netlify.app/'
+  'https://main--effortless-custard-7e0ce8.netlify.app'
 ]
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
